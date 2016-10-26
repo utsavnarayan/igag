dad255dcb34e6f49a78e15fc198446d2c4a0d52f
Utsav Singh <utsavinbox@gmail.com>
2016-10-26 18:03:53 +0530

    added swagger

--- igag/urls.py
+++ igag/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
 from service import views
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='iGag API')
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -26,7 +30,8 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
 ]
