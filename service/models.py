2c6f92d40e07adb3901f0dafe958489f5ae9f66a
Utsav Singh <utsavinbox@gmail.com>
2016-10-26 18:21:12 +0530

    Added model for a Post

--- service/models.py
+++ service/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    text = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
